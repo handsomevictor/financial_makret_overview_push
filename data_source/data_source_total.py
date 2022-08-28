be87c78554af1b8699c880f52a1182ee49618cf9
handsomevictor <857730556@qq.com>
2022-08-28 12:56:09 +0200

    updated data source

--- data_source/data_source_total.py
+++ data_source/data_source_total.py
@@ -31,52 +31,52 @@ us_sp500_tickers = pd.read_csv(us_sp500_tickers_csv_url)['Symbol'].to_list()
 # 定义一个常用US ticker，大大减少时间，在这些ticker执行完之后，再去执行其他的全部，分两次发邮件，第一个叫：major market move，第二个叫
 # comprehensive market overview
 def us_common_tickers(us_nasdaq_ticker_list, us_other_ticker_list, us_sp500_tickers):
-    # 先判断是否已经下载了common tickers，如果有，直接读取并return，没有则重新读取上一次运行完之后的两万多个json文件提取common tickers！
-    # 如果这个文件是5天前的，则重新下载！
-    try:
-        if platform.system() == 'Linux':
-            file_loc = os.path.join(os.getcwd(), 'financial_makret_overview_push', 'temp_database_for_convenience',
-                                    'us_common_tickers.pkl')
-        else:
-            file_loc = os.path.join(os.getcwd(), 'temp_database_for_convenience', 'us_common_tickers.pkl')
+    for _ in range(3):
+        try:
+            # 先判断是否已经下载了common tickers，如果有，直接读取并return，没有则重新读取上一次运行完之后的两万多个json文件提取common tickers！
+            # 如果这个文件是5天前的，则重新下载！
+            try:
+                if platform.system() == 'Linux':
+                    file_loc = os.path.join(os.getcwd(), 'financial_makret_overview_push', 'temp_database_for_convenience',
+                                            'us_common_tickers.pkl')
+                else:
+                    file_loc = os.path.join(os.getcwd(), 'temp_database_for_convenience', 'us_common_tickers.pkl')
 
-        # 查看文件创建时间
-        m_time = os.path.getmtime(file_loc)
-        dt_m = datetime.datetime.fromtimestamp(m_time)
-        days_diff = (datetime.datetime.now() - dt_m).days
-        if days_diff < 3:
-            with open(file_loc, 'rb') as f:
-                common_tickers = pickle.load(f)
-                print('Common tickers file exists, data loaded!')  # 在多线程的时候，这句话会被打印n多次！
-            return common_tickers
-        else:
-            raise FileNotFoundError
+                # 查看文件创建时间
+                m_time = os.path.getmtime(file_loc)
+                dt_m = datetime.datetime.fromtimestamp(m_time)
+                days_diff = (datetime.datetime.now() - dt_m).days
+                if days_diff < 3:
+                    with open(file_loc, 'rb') as f:
+                        common_tickers = pickle.load(f)
+                        print('Common tickers file exists, data loaded!')  # 在多线程的时候，这句话会被打印n多次！
+                    return common_tickers
+                else:
+                    raise FileNotFoundError
 
-    except FileNotFoundError:
-        # 开始重新获取九千多个uncommon tickers的数据！
-        print('Common tickers file expired or does not exists, common and uncommon tickers processed data redownloading!')
+            except FileNotFoundError:
+                # 开始重新获取九千多个uncommon tickers的数据！
+                print('Common tickers file expired or does not exists, common and uncommon tickers processed data redownloading!')
 
-        # 重新下载uncommon文件！
-        # 这里没有下载！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！
+                # 重新下载uncommon文件！
+                # 这里没有下载！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！
 
-        if platform.system() == 'Linux':
-            file_name = os.path.join(os.getcwd(), 'financial_makret_overview_push', 'temp_database_for_convenience',
-                                     'us_uncommon_tickers_processed_result.pkl')
-        else:
-            file_name = os.path.join(os.getcwd(), 'temp_database_for_convenience', 'us_uncommon_tickers_processed_result.pkl')
+                if platform.system() == 'Linux':
+                    file_name = os.path.join(os.getcwd(), 'financial_makret_overview_push', 'temp_database_for_convenience',
+                                             'us_uncommon_tickers_processed_result.pkl')
+                else:
+                    file_name = os.path.join(os.getcwd(), 'temp_database_for_convenience', 'us_uncommon_tickers_processed_result.pkl')
 
-        with open(file_name, 'rb') as f:
-            tickers_processed_result = pickle.load(f)
+                with open(file_name, 'rb') as f:
+                    tickers_processed_result = pickle.load(f)
 
-        return tickers_processed_result.ticker.to_list()
+                return tickers_processed_result.ticker.to_list()
+        except:
+            print('Retrying to get total ticker list because of HTTP Error 502: Bad Gateway...')
+            continue
 
 
-for _ in range(3):
-    try:
-        us_common_total_ticker_list = us_common_tickers(us_nasdaq_ticker_list, us_other_ticker_list, us_sp500_tickers).ticker.to_list()
-    except:
-        print('Retrying to get total ticker list because of HTTP Error 502: Bad Gateway...')
-        continue
+us_common_total_ticker_list = us_common_tickers(us_nasdaq_ticker_list, us_other_ticker_list, us_sp500_tickers).ticker.to_list()
 
 
 if __name__ == '__main__':
