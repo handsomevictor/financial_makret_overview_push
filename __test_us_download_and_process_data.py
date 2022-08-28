1f6408818d413c04985f088dc118b7ee795aa73b
handsomevictor <857730556@qq.com>
2022-08-29 00:51:27 +0200

    updated requirement list

--- __test_us_download_and_process_data.py
+++ __test_us_download_and_process_data.py
@@ -8,6 +8,7 @@ from tools.time_decorator import timeit
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 
+
 from data_processing.process_individual_basic_data import calculate_individual_stock_single_result
 from data_processing.process_individual_US_get_cap_and_industry import US_data_get_cap_and_industry
 from data_source.data_source_total import us_common_total_ticker_list
